733fb4b8bae10cf3abfa753fedf6941bab8110d5
msmadhu019 <msmadhu019@gmail.com>
2024-06-02 01:16:26 +0530

    Day1

---
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/service/impl/PostService.java
@@ -0,0 +1,11 @@
+package com.springboot.blog.service.impl;
+
+import com.springboot.blog.dto.PostDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+    public PostDto createPost(PostDto postDto) {
+        return null;
+    }
+}
